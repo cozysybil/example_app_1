d624b745ce272106fc2eca61e3bf63266095b856
Sybil <cozysybil@gmail.com>
2023-01-21 21:39:16 +0700

    feat: add new page

---
+++ lib/container/containerPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ContainerPage extends StatefulWidget {
+  const ContainerPage({Key? key}) : super(key: key);
+
+  @override
+  State<ContainerPage> createState() => _ContainerPageState();
+}
+
+class _ContainerPageState extends State<ContainerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
